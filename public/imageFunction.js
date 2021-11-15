990ca6970def4eb0f81383a55641f68bd96108d7
Samuel Hauptmann van Dam <36303155+SamuelHauptmannvanDam@users.noreply.github.com>
2021-11-15 21:12:17 +0100

    all good?

--- public/imageFunction.js
+++ public/imageFunction.js
@@ -24,6 +24,7 @@ export function imagesFunction(centeringBox) {
     pictureFrame.appendChild(pictureFrameSmall);
 
     const imgSmall1 = document.createElement("img");
+    imgSmall1.className = "imgSmall"
     imgSmall1.onclick = function() { window.open("https://www.airbnb.dk/rooms/50240587?translate_ugc=false&federated_search_id=bf966e2b-73a5-45f9-a58c-e2c02d76cc21&source_impression_id=p3_1626532448_dH7cpFG6Qv0cSTMe&guests=1&adults=1"); };
     imgSmall1.src = "images/4.jpg";
     imgSmall1.style.width = "48%"
@@ -32,6 +33,7 @@ export function imagesFunction(centeringBox) {
 
 
     const imgSmall2 = document.createElement("img");
+    imgSmall2.className = "imgSmall"
     imgSmall2.onclick = function() { window.open("https://www.airbnb.dk/rooms/50245477?translate_ugc=false&federated_search_id=bf966e2b-73a5-45f9-a58c-e2c02d76cc21&source_impression_id=p3_1626532449_qutief9GI1VhK5Ii&guests=1&adults=1"); };
     imgSmall2.src = "images/1.jpg";
     imgSmall2.style.width = "48%"
@@ -40,6 +42,7 @@ export function imagesFunction(centeringBox) {
 
 
     const imgSmall3 = document.createElement("img");
+    imgSmall3.className = "imgSmall"
     imgSmall3.onclick = function() { window.open("https://www.facebook.com/Lindegaardenbnb/photos/?ref=page_internal"); };
     imgSmall3.src = "images/2.jpg";
     imgSmall3.style.width = "48%"
@@ -48,6 +51,7 @@ export function imagesFunction(centeringBox) {
 
 
     const imgSmall4 = document.createElement("img");
+    imgSmall4.className = "imgSmall"
     imgSmall4.onclick = function() { window.open("https://www.airbnb.dk/rooms/50003907?translate_ugc=false&federated_search_id=bf966e2b-73a5-45f9-a58c-e2c02d76cc21&source_impression_id=p3_1626532449_F3oSvw5Pw%2BacT%2BOu&guests=1&adults=1"); };
     imgSmall4.src = "images/5.jpg";
     imgSmall4.style.width = "48%"
--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+document.cookie = 'cookie2=value2; SameSite=None; Secure';
+
 //MAIN BOX
 const flexBox = document.createElement('flexBox');
 flexBox.className = "fade-in"
@@ -55,8 +57,8 @@ fetch('https://api.geoapify.com/v1/ipinfo?apiKey=fb0e13a6fe3b4773ab9d95edbbd12e7
     .then((userLocationData) => {
         if (userLocationData.country.name == "Denmark") {
             toDanish()
-
         } else {
             toEnglish()
         }
     });
+
