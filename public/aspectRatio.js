37d6cfcbcef9894856e12f4589eda32fdf064866
Samuel <samuelhauptmannvandam@gmail.com>
2022-03-16 13:54:40 +0100

    added tiny fade back

--- public/aspectRatio.js
+++ public/aspectRatio.js
@@ -5,25 +5,9 @@ export function checkScreenAspect() {
         let aspectFactor = w / h;
 
         if (aspectFactor > 1.2) {
-            // document.getElementById("CenteringBoxID").style.width = "40%";
-            // document.getElementById("pictureFrameID").style.flexDirection = "row";
-            // document.getElementById("pictureFrameLargeID").style.width = "100%";
-            // document.getElementById("pictureFrameSmallID").style.width = "100%";
-            // document.getElementById("bookingFrameID").style.flexDirection = "row";
-            // document.getElementById("bookingFrameDescriptionID").style.width = "50%";
-            // document.getElementById("bookingFrameCostID").style.width = "50%";
-            // document.getElementById("bookingBottomButtonID").style.width = "30%";
             document.getElementById("iframID").height = document.getElementById('mapID').getBoundingClientRect().width;
 
         } else { // UI changes for phones / screens taller than wide.
-            // document.getElementById("CenteringBoxID").style.width = "100%";
-            // document.getElementById("pictureFrameID").style.flexDirection = "column";
-            // document.getElementById("pictureFrameLargeID").style.width = "100%";
-            // document.getElementById("pictureFrameSmallID").style.width = "100%";
-            // document.getElementById("bookingFrameID").style.flexDirection = "column-reverse";
-            // document.getElementById("bookingFrameDescriptionID").style.width = "100%";
-            // document.getElementById("bookingFrameCostID").style.width = "100%";
-            // document.getElementById("bookingBottomButtonID").style.width = "80%";
             document.getElementById("iframID").height = document.getElementById('mapID').getBoundingClientRect().width;
         }
     }
